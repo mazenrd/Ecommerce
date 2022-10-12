d61e0d1da7a9aba3f11ce21a6f33fd637703c4bd
mazenrd <mazenrida98@gmail.com>
2022-10-12 05:51:09 +0100

    features/ProductDetails

---
+++ components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+import { Navbar, Footer } from "../components";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>Mazen Store</Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Headphones</Link>
+      </p>
+
+      <button
+        type="button"
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className="cart-item-qty">test</span>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
